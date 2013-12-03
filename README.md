e998c89a5ee85914079da5ae4e92b24f66507cfb
uaraven <ovoronin@gmail.com>
2013-12-02 20:00:59 -0500

    Fixed filtering

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 drizzle
 =======
 
-Weather indicator for Ubuntu
+Weather indicator for Ubuntu.
+
+Not really works for Gnome 3, but should be fine for Unity.
+Misses configuration UI.
+
+
+Dependencies
+------------
+
+* python-appindicator
+* python-gtk2
+
+
--- drizzle.py
+++ drizzle.py
@@ -1,11 +1,13 @@
 # coding=utf-8
 import sys
 import os
+import datetime
+import ConfigParser
+
 import gtk
 import appindicator
-import pywapi
-import datetime
 
+import pywapi
 
 CONFIG_PATH = '.config/drizzle'
 
@@ -17,28 +19,43 @@ CONDITION = 'condition'
 
 class Config:
     def __init__(self):
-        self.config = os.path.join(os.path.expanduser('~'), CONFIG_PATH)
+        self.config_file = os.path.join(os.path.expanduser('~'), CONFIG_PATH)
+        self.config = ConfigParser.SafeConfigParser()
+        if os.path.exists(self.config_file):
+            self.config.read(self.config_file)
 
     def is_location_present(self):
-        return self.get_locations() is not None
+        return self.config.has_section('location')
 
     def get_locations(self):
-        loc_file = os.path.join(self.config, 'location')
-        if os.path.exists(loc_file):
-            with open(loc_file) as inp:
-                location = inp.readlines()
-                return [x.strip() for x in location]
+        if self.config.has_section('location'):
+            return [x[1] for x in self.config.items('location')]
         else:
-            return None
+            return [u'CAXX0295']
+
+    def get_update_interval(self):
+        if self.config.has_option('settings', 'update'):
+            timeout_secs = self.config.getint('settings', 'update')
+        else:
+            timeout_secs = 60 * 120
+        return timeout_secs * 1000
 
 
 class Forecast:
     def __init__(self, yahoo_forecast):
         self.code = yahoo_forecast['code']
-        self.date = datetime.date.strptime(yahoo_forecast['date'], '%d %b %Y')
+        date = yahoo_forecast['date'].split(' ')
+        self.date = datetime.date(int(date[2]), self.get_month(date[1]), int(date[0]))
         self.day = yahoo_forecast['day']
         self.high = yahoo_forecast['high']
         self.low = yahoo_forecast['low']
+
+    def get_month(self, param):
+        return {'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5, 'Jun': 6, 'Jul': 7, 'Aug': 8, 'Sep': 9,
+                'Oct': 10, 'Nov': 11, 'Dec': 12}[param]
+
+    def not_today(self):
+        return self.date != datetime.date.today()
 
 
 class CurrentWeather:
@@ -68,23 +85,14 @@ class CurrentWeather:
 class WeatherIndicator:
     UNKNOWN_WEATHER = "na"
 
-    def __init__(self, locations):
+    def __init__(self):
 
         self_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "icons")
-
-        self.update_interval = 3
-        self.weather = {}
 
         self.indicator = appindicator.Indicator("Weather", self.UNKNOWN_WEATHER, appindicator.CATEGORY_OTHER, )
         self.indicator.set_icon_theme_path(self_path)
         self.indicator.set_status(appindicator.STATUS_ACTIVE)
-        self.menu_setup()
-
-        self.temperature_unit = ''
-
-        gtk.timeout_add(self.update_interval * 60 * 1000, self.update_weather)
-        self.locations = locations
-        self.id = locations[0]
+        self.update_indicator_menu(None)
 
     def create_menu_item(self, text):
         menu_item = gtk.MenuItem(text)
@@ -92,26 +100,25 @@ class WeatherIndicator:
         menu_item.show()
         return menu_item
 
-    def menu_setup(self):
+    def update_indicator_menu(self, weather):
         self.menu = gtk.Menu()
 
-        if len(self.weather) != 0:
-            self.menu.append(self.create_menu_item(self.location_name))
+        if weather is not None:
+            self.menu.append(self.create_menu_item(weather.location_name))
             self.menu.append(self.create_menu_item(None))
-            self.menu.append(self.create_menu_item(self.weather[NOW]['text']))
+            self.menu.append(self.create_menu_item(weather.text))
             self.menu.append(self.create_menu_item(
-                "Temperature: " + self.weather[NOW]['temp'] + DEGREE + self.temperature_unit))  # TODO: Localize
-            self.menu.append(self.create_menu_item('Feels like: ' + self.weather[NOW]['wind_chill'] + DEGREE
-                                                   + self.temperature_unit))
-            self.menu.append(self.create_menu_item('Humidity: ' + self.weather[NOW]['humidity'] + '%'))
-            self.menu.append(self.create_menu_item('Pressure: ' + self.weather[NOW]['pressure'] + ' '
-                                                   + self.pressure_unit))
-            if len(self.weather[NOW]['visibility']) > 0:
-                self.menu.append(self.create_menu_item('Visibility: ' + self.weather[NOW]['visibility'] + ' '
-                                                       + self.distance_unit))
-            pass
+                "Temperature: " + weather.temp + DEGREE + weather.temperature_unit))  # TODO: Localize
+            self.menu.append(self.create_menu_item('Feels like: ' + weather.wind_chill + DEGREE
+                                                   + weather.temperature_unit))
+            self.menu.append(self.create_menu_item('Humidity: ' + weather.humidity + '%'))
+            self.menu.append(self.create_menu_item('Pressure: ' + weather.pressure + ' '
+                                                   + weather.pressure_unit))
+            if len(weather.visibility) > 0:
+                self.menu.append(self.create_menu_item('Visibility: ' + weather.visibility + ' '
+                                                       + weather.distance_unit))
             self.menu.append(self.create_menu_item(None))
-        self.quit_item = gtk.MenuItem("Quit") # TODO: Localize
+        self.quit_item = gtk.MenuItem("Quit")  # TODO: Localize
         self.quit_item.connect("activate", self.quit)
         self.quit_item.show()
         self.menu.append(self.quit_item)
@@ -121,59 +128,39 @@ class WeatherIndicator:
     def quit(self, widget):
         sys.exit(0)
 
-    def set_location(self, location, lookup=False):
-        if lookup:
-            ids = pywapi.get_location_ids(location)
-            for k in ids:
-                self.id = k
-                self.location_name = ids[k]
-                break
-        else:
-            self.id = location
-            self.location_name = ''
-
-        self.update_weather()
-
-    def update_weather(self):
-        weather = pywapi.get_weather_from_yahoo(self.id)
-        print 'Updated @', datetime.datetime.today()
-
-        self.weather = {NOW: {'condition_code': weather[CONDITION]['code'], 'temp': weather[CONDITION]['temp'],
-                              'text': weather[CONDITION]['text']}}
-
-        self.weather[NOW]['wind_chill'] = weather['wind']['chill']
-        self.weather[NOW]['wind_direction'] = weather['wind']['direction']
-        self.weather[NOW]['wind_speed'] = weather['wind']['speed']
-
-        self.weather[NOW]['humidity'] = weather[ATMOSPHERE]['humidity']
-        self.weather[NOW]['pressure'] = weather[ATMOSPHERE]['pressure']
-        self.weather[NOW]['pressure_rising'] = weather[ATMOSPHERE]['rising']
-        self.weather[NOW]['visibility'] = weather[ATMOSPHERE]['visibility']
-
-        self.location_name = weather['location']['city'] + ', ' + weather['location']['country']
-        self.title = weather[CONDITION]['title']
-
-        self.indicator.set_icon(weather[CONDITION]['code'])
-
-        self.temperature_unit = weather['units']['temperature']
-        self.distance_unit = weather['units']['distance']
-        self.pressure_unit = weather['units']['pressure']
-        self.speed_unit = weather['units']['speed']
-
-        self.menu_setup()
-        gtk.timeout_add(self.update_interval * 60 * 1000, self.update_weather)
-        return False
+    def update_weather(self, weather):
+        self.indicator.set_icon(weather.condition_code)
+        if weather is not None:
+            self.update_indicator_menu(weather)
 
 
 class Drizzle:
     def __init__(self):
         self.config = Config()
         self.indicator = WeatherIndicator()
+        self.weather = None
+
+        gtk.timeout_add(self.config.get_update_interval(), self.update_weather)
+        self.update_weather()
+
+    def update_weather(self):
+        try:
+            locations = self.config.get_locations()
+            weather = pywapi.get_weather_from_yahoo(locations[0])
+            self.weather = CurrentWeather(weather)
+            self.forecasts = filter(self.not_today, [Forecast(x) for x in weather['forecasts']])
+
+            self.indicator.update_weather(self.weather)
+        except Exception as e:
+            print e
+            self.weather = None
+
+        return True
+
+    def not_today(self, forecast):
+       return forecast.not_today()
 
 
 if __name__ == '__main__':
-    c = Config()
-    c.get_location()
-    wi = WeatherIndicator()
-    wi.set_location(u'CAXX0295')
+    drizzle = Drizzle()
     gtk.main()
